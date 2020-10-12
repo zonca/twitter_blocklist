66d5169ed1e9851a9f417593dfa1cd6baa43941d
Andrea Zonca <code@andreazonca.com>
2020-10-12 11:31:08 -0700

    support environment variables

--- src/twitter_blocklist/utils.py
+++ src/twitter_blocklist/utils.py
@@ -1,3 +1,4 @@
+import os
 import twitter
 import toml
 
@@ -7,5 +8,15 @@ def authenticate():
 
     it uses the ``twitter_keys.toml`` file in the current folder
     """
-    api = twitter.Api(**toml.load("twitter_keys.toml"), sleep_on_rate_limit=True)
+    if "TWITTER_CONSUMER_KEY" in os.environ:
+        keys = [
+            "consumer_key",
+            "consumer_secret",
+            "access_token_key",
+            "access_token_secret",
+        ]
+        auth_dict = {k: os.environ["TWITTER_" + k.upper()] for k in keys}
+    else:
+        auth_dict = toml.load("twitter_keys.toml")
+    api = twitter.Api(**auth_dict, sleep_on_rate_limit=True)
     return api
