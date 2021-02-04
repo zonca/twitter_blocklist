249eea6140c5437ad3b4241d6cb899e5ee60ae52
Andrea Zonca <code@andreazonca.com>
2021-02-04 09:20:36 -0800

    support auth via arguments

--- src/twitter_blocklist/utils.py
+++ src/twitter_blocklist/utils.py
@@ -3,20 +3,32 @@ import twitter
 import toml
 
 
-def authenticate():
+def authenticate(auth_dict=None):
     """Authenticate to the Twitter API
 
-    it uses the ``twitter_keys.toml`` file in the current folder
+    It supports 3 ways of authenticating (in this order):
+    * pass the auth_dict dictionarie as input argument with keys:
+            consumer_key,consumer_secret,access_token_key,access_token_secret
+    * set the environment variables:
+            TWITTER_CONSUMER_KEY,TWITTER_CONSUMER_SECRET,TWITTER_ACCESS_TOKEN_KEY,
+            TWITTER_ACCESS_TOKEN_SECRET
+    * loads a ``twitter_keys.toml`` file in the current folder
+
+    Returns
+    -------
+    api : twitter.Api
+        Authenticated twitter API instance
     """
-    if "TWITTER_CONSUMER_KEY" in os.environ:
-        keys = [
-            "consumer_key",
-            "consumer_secret",
-            "access_token_key",
-            "access_token_secret",
-        ]
-        auth_dict = {k: os.environ["TWITTER_" + k.upper()] for k in keys}
-    else:
-        auth_dict = toml.load("twitter_keys.toml")
+    if auth_dict is None:
+        if "TWITTER_CONSUMER_KEY" in os.environ:
+            keys = [
+                "consumer_key",
+                "consumer_secret",
+                "access_token_key",
+                "access_token_secret",
+            ]
+            auth_dict = {k: os.environ["TWITTER_" + k.upper()] for k in keys}
+        else:
+            auth_dict = toml.load("twitter_keys.toml")
     api = twitter.Api(**auth_dict, sleep_on_rate_limit=True)
     return api
